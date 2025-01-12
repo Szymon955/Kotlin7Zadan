36b30bf2828bf4a9bc3aa5a3e283aae48b5ea28d
Szymon <gutek.gutomski@gmail.com>
2025-01-12 23:16:13 +0100

    Object, methods

---
+++ Zadanie2/app/src/main/java/RandomQuoteGenerator.kt
@@ -0,0 +1,22 @@
+// Singleton
+object Quotes {
+    // Lista cytatów
+    private val quotesList = mutableListOf(
+        "Ciężka praca popłaca",
+        "Lewy to maszyna",
+        "Kotlin to super język",
+
+    )
+
+    // Metoda zwracająca losowy cytat
+    fun getRandomQuote(): String {
+        return quotesList.random()
+    }
+
+    // Metoda dodająca nowy cytat do bazy
+    fun addQuote(newQuote: String) {
+        quotesList.add(newQuote)
+    }
+}
+
+
