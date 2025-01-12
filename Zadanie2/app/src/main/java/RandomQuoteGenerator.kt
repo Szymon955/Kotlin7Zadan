ccc966b0ddb52b962bef4bc886d10f07596f9584
Szymon <gutek.gutomski@gmail.com>
2025-01-12 23:34:06 +0100

    add main function

--- Zadanie2/app/src/main/java/RandomQuoteGenerator.kt
+++ Zadanie2/app/src/main/java/RandomQuoteGenerator.kt
@@ -16,3 +16,35 @@ object Quotes {
         quotesList.add(newQuote)
     }
 }
+
+fun main() {
+    while (true) {
+        println("\nWybierz opcję:")
+        println("1. Wylosuj cytat")
+        println("2. Dodaj nowy cytat")
+        println("3. Wyjście")
+
+        when (readLine()?.toIntOrNull()) {
+            1 -> {
+                println("\nLosowy cytat: ${Quotes.getRandomQuote()}")
+            }
+            2 -> {
+                print("\nWpisz nowy cytat: ")
+                val newQuote = readLine()
+                if (!newQuote.isNullOrBlank()) {
+                    Quotes.addQuote(newQuote)
+                    println("Nowy cytat został dodany.")
+                } else {
+                    println("Cytat nie może być pusty.")
+                }
+            }
+            3 -> {
+                println("Do widzenia!")
+                break
+            }
+            else -> {
+                println("Nieprawidłowy wybór. Spróbuj ponownie.")
+            }
+        }
+    }
+}
